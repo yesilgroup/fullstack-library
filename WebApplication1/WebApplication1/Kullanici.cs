04817fdebf92024a7ca1042f0095a5b3de0858d1
Ezgi <ezgycl28@gmail.com>
2024-08-02 22:10:47 +0300

    kullanici sınıfı ekleme

--- WebApplication1/WebApplication1/Kullanici.cs
+++ WebApplication1/WebApplication1/Kullanici.cs
@@ -8,5 +8,15 @@
         public string Eposta { get; set; }
         public string Sifre { get; set; }
         public DateTime KayitTarihi { get; set; } = DateTime.Now;
+
+        public Kullanici(int kullaniciID, string ad, string soyad, string email, string sifre,DateTime kayitTarihi)
+        {
+            this.Id = kullaniciID;
+            this.Ad = ad;
+            this.Soyad = soyad;
+            this.Eposta = email;
+            this.Sifre = sifre;
+            this.KayitTarihi = kayitTarihi;
+        }
     }
 }
