e4cc1d94af78cf033a88c535ef4aa4bb3a025bcc
Ezgi <ezgycl28@gmail.com>
2024-08-02 21:49:49 +0300

    kullanici sinifi tanımladım

---
+++ WebApplication1/WebApplication1/Kullanici.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1
+{
+    public class Kullanici
+    {
+        public int Id { get; set; }
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string Eposta { get; set; }
+        public string Sifre { get; set; }
+        public DateTime KayitTarihi { get; set; } = DateTime.Now;
+    }
+}
