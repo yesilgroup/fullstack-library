72a79ad65ebb469d2e80f4f9696d86e847837e39
Ezgi <ezgycl28@gmail.com>
2024-08-02 22:20:55 +0300

    Uye sinifindan gorevli türettim

---
+++ WebApplication1/WebApplication1/Gorevli.cs
@@ -0,0 +1,10 @@
+﻿namespace WebApplication1
+{
+    public class Gorevli : Uye
+    {
+        public Gorevli(int kullaniciID, string ad, string soyad, string email, string sifre, DateTime kayitTarihi) : base(kullaniciID, ad, soyad, email, sifre, kayitTarihi)
+        {
+
+        }
+    }
+}
