ad30457111baa019b9a462eef562f20bd58bfdf6
Ezgi <ezgycl28@gmail.com>
2024-08-02 22:17:19 +0300

    kullanici sinifindan uye türettim

---
+++ WebApplication1/WebApplication1/Uye.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1
+{
+    public class Uye : Kullanici
+    {
+        public Uye(int kullaniciID, string ad, string soyad, string email, string sifre,DateTime kayitTarihi) : base(kullaniciID, ad, soyad, email, sifre,kayitTarihi)
+        {
+        }
+
+
+    }
+}
